a501611c395585b381cc8af4b26aeb53a385fa15
aprochoi <ym24319577@gmail.com>
2022-11-27 16:15:48 +0900

    HamburgerSet 클래스 작성

---
+++ src/app/product/HamburgerSet.java
@@ -0,0 +1,26 @@
+package app.product;
+
+public class HamburgerSet extends Product {
+    private Hamburger hamburger;
+    private Side side;
+    private Drink drink;
+
+    public HamburgerSet(int id, String name, int price, int kcal, Hamburger hamburger, Side side, Drink drink) {
+        super(id, name, price, kcal);
+        this.hamburger = hamburger;
+        this.side = side;
+        this.drink = drink;
+    }
+
+    public Hamburger getHamburger() {
+        return hamburger;
+    }
+
+    public Side getSide() {
+        return side;
+    }
+
+    public Drink getDrink() {
+        return drink;
+    }
+}
