8517a4a453aa186daf503dd7bc370017aa19bd2b
aprochoi <ym24319577@gmail.com>
2022-11-27 16:13:31 +0900

    Drink 클래스 작성

---
+++ src/app/product/Drink.java
@@ -0,0 +1,18 @@
+package app.product;
+
+public class Drink extends Product {
+    private boolean straw;
+
+    public Drink(int id, String name, int price, int kcal, boolean straw) {
+        super(id, name, price, kcal);
+        this.straw = straw;
+    }
+
+    public boolean isStraw() {
+        return straw;
+    }
+
+    public void setStraw(boolean straw) {
+        this.straw = straw;
+    }
+}
