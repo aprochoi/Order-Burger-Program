e228608d233776cd04ce9789a05c7d21f8f19cab
aprochoi <ym24319577@gmail.com>
2022-12-05 15:24:07 +0900

    메뉴 기능 구현

--- src/burger/Menu.java
+++ src/burger/Menu.java
@@ -1,11 +1,64 @@
 package burger;
 
 import burger.product.Product;
+import burger.product.detailproduct.Drink;
+import burger.product.detailproduct.Hamburger;
+import burger.product.detailproduct.Side;
 
 public class Menu {
     private Product[] products;
 
     public Menu(Product[] products) {
         this.products = products;
+    }
+
+    public void showMenu() {
+        System.out.println("[ 🔻 ] 메뉴");
+        System.out.println("-".repeat(50));
+
+        showHamburgers();
+        showSides();
+        showDrinks();
+
+        System.out.println();
+        System.out.println("🧺 (0) 장바구니");
+        System.out.println("🛒 (+) 주문하기");
+        System.out.println("-".repeat(50));
+        System.out.println("[안내] 메뉴를 선택하세요 : ");
+    }
+
+    private void showDrinks() {
+        System.out.println("🥤 음료수");
+        for (Product product : products) {
+            if(product instanceof Drink) {
+                printMenu(product);
+            }
+        }
+        System.out.println();
+    }
+
+    private void showSides() {
+        System.out.println("🍟 사이드");
+        for (Product product : products) {
+            if (product instanceof Side) {
+                printMenu(product);
+            }
+        }
+        System.out.println();
+    }
+
+    private void showHamburgers() {
+        System.out.println("🍔 햄버거");
+        for (Product product : products) {
+            if(product instanceof Hamburger) {
+                printMenu(product);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printMenu(Product product) {
+        System.out.printf(" (%d) %s %sKcal %s원\n",
+                product.getId(), product.getName(), product.getKcal(), product.getPrice());
     }
 }
--- src/burger/OrderBurger.java
+++ src/burger/OrderBurger.java
@@ -3,13 +3,18 @@ package burger;
 import burger.product.Product;
 import burger.product.ProductRepository;
 
+import java.util.Scanner;
+
 public class OrderBurger {
 
     public void run() {
+        Scanner sc = new Scanner(System.in);
         ProductRepository productRepository = new ProductRepository();
         Product[] products = productRepository.getProducts();
         Menu menu = new Menu(products);
 
         System.out.println("🍔 햄버거 주문 프로그램");
+        menu.showMenu();
+        String choice = sc.nextLine();
     }
 }
